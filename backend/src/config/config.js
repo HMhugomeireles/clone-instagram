bd485d1f8033d44430e9a4aaa7ebe8e1f6aa395f
Hugo Meireles <meireles.hmm@gmail.com>
2019-06-23 18:36:42 +0100

    create backend

---
+++ backend/src/config/config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  CONNECTING_MONGODB: `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0-mm0ak.mongodb.net/test?retryWrites=true&w=majority`
+}
---
+++ backend/src/index.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const Config = require('./config/config');
+const path = require('path');
+const cors = require('cors');
+
+const app = express();
+
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+mongoose.connect(Config.CONNECTING_MONGODB, {
+  useNewUrlParser: true 
+});
+
+app.use((req, res, next) => {
+  req.io = io;
+  next();
+});
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')));
+app.use(cors());
+app.use(require('./routes'));
+
+
+server.listen(3333);
