382bfde8b43b2599e0676ee1a4249036084e3dcc
Hugo Meireles <meireles.hmm@gmail.com>
2019-06-24 00:58:31 +0100

    create frontend in react

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './header.css';
+import logo from '../assets/logo.svg';
+import camera from '../assets/camera.svg';
+
+export default function Header() {
+  return (
+    <header id="main-header">
+      <div className="header-content">
+        <Link to="/">
+          <img src={logo} alt="clone instagram"></img>
+        </Link>
+        <Link to="/new">
+          <img src={camera} alt="Send post"></img>
+        </Link>
+      </div>
+    </header>
+  );
+}
