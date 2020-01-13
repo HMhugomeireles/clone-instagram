74d326118a64d0f381812c8f1ed0b475bf51545b
Hugo Meireles <meireles@LT129408.novabase.intra>
2020-01-13 20:06:47 +0000

    change naming

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,20 @@
-require('dotenv').config();
 const express = require('express');
-const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const Config = require('./config/config');
 const path = require('path');
 const cors = require('cors');
 
 const app = express();
 
-const SERVER_PORT = process.env.PORT || 3333;
-
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
+
+const Config = require('./config/config');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+
+const SERVER_PORT = process.env.PORT || 3333;
+
 
 mongoose.connect(Config.CONNECTING_MONGODB, {
   useNewUrlParser: true,
@@ -30,5 +33,5 @@ app.use(require('./routes'));
 
 
 server.listen(SERVER_PORT, () => {
-  console.log(`Server is runing in port:${SERVER_PORT}`)
+  console.log(`Server is running in port:${SERVER_PORT}`)
 });
